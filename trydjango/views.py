d486bd2c65c3e8cd7a3910ea0b8624def93ed315
sanjay17s <sanjay.17suresh2000@gmail.com>
2024-08-06 12:45:22 +0530

    django templates

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,22 +1,35 @@
-'''the goal of this file is to render html web pages
-from my understanding we have some url say ..../home
-when this is requested by the user
-in django some file will catch this request,i mean some mapping will be 
-there to service this kind of request
-request will have some metadata 
-and this function which acts as a receiver of this request routes this to views.py'''
+'''The `views.py` file in a Django project is responsible for rendering HTML 
+web pages. When a user requests a specific URL (e.g., `/home`), Django maps the
+ URL to a function in `views.py`. This function receives the request,
+  processes it, and returns the appropriate HTML response.'''
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from articles.models import Article
 import random
+
 
 def home(request):
     # This function takes in a request and then returns HTML as Response
-    name = "sanjay"
-    num = random.randint(10, 7899)
-    HTML_STRING = f"""
-    <h1>Hello {name.title()} - {num}</h1>"""
-    return HttpResponse(HTML_STRING)
+    try:
+        article = Article.objects.get(id=3)
+    except Article.DoesNotExist:
+        # Handle the case where the article does not exist
+        context = {
+            "title": "Article Not Found",
+            "content": "The article you are looking for does not exist.",
+            "id": None  
+        }
+        return render(request, 'myapp/some_file.html', context)
+   
+    context = {
+        "title": article.title,
+        "content": article.content,
+        "id": article.id
+    }
+ 
+    return render(request, 'myapp/some_file.html', context)
+
 
 def abc(request):
     res = "hignkfs"
